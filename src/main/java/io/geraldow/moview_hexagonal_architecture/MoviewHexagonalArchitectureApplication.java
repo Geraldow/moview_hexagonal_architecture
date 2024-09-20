20a2be10f9ef1d7b342bd0723e39c50c67790d68
Geraldo Jaramillo <Geraldo_07_@outlook.com>
2024-09-19 23:14:08 -0500

    add: agregando proyecto base de moview

---
+++ src/main/java/io/geraldow/moview_hexagonal_architecture/MoviewHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package io.geraldow.moview_hexagonal_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviewHexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviewHexagonalArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/geraldow/moview_hexagonal_architecture/MoviewHexagonalArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.geraldow.moview_hexagonal_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewHexagonalArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
